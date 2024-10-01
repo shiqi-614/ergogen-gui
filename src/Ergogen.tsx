6d2cc548ae8b4e2b185d153471e94c338cb10044
shiqi614 <shiqi614@gmail.com>
2024-10-01 19:19:55 +0800

    remove useless code

--- src/Ergogen.tsx
+++ src/Ergogen.tsx
@@ -10,7 +10,6 @@ import {useConfigContext} from "./context/ConfigContext";
 import Button from "./atoms/Button";
 import Select from "react-select";
 import GenOption from "./atoms/GenOption";
-import Tabs from "./organisms/Tabs";
 import {ConfigOption, exampleOptions} from "./examples";
 
 const EditorContainer = styled.div`
